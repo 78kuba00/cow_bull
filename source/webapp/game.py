86fa3c006832d95814cd3d7222a1bf88cd55e9dc
Zhantorouulu_Kubat <78kuba00@gmail.com>
2022-10-29 22:52:57 +0600

    Третий коммит: добавил game.py и модифицировал urls.py, index.html, views.py

---
+++ source/webapp/game.py
@@ -0,0 +1,31 @@
+class Game:
+    def play(number_guess_list, number_computer_list):
+        cow_numbers, bull_numbers = [], []
+        try:
+            for index in range(len(number_guess_list)):
+                if number_computer_list[index] == int(number_guess_list[index]):
+                    bull_numbers.append(index)
+                elif int(number_guess_list[index]) in number_computer_list:
+                    cow_numbers.append(index)
+            return cow_numbers, bull_numbers
+        except:
+            return cow_numbers, bull_numbers
+
+    def is_valid_count(number_list):
+        if len(number_list) == 4:
+            return True
+        return False
+
+    def is_unique_numbers(number_list):
+        s = set()
+        for x in number_list:
+            if x in s:
+                return False
+            s.add(x)
+        return True
+
+    def is_valid_range(number_list):
+        for x in number_list:
+            if int(x) < 1 or int(x) > 9:
+                return False
+            return True
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
+from .game import Game
 
 # Create your views here.
-def play_view(request):
-    return render(request, "index.html")
+def play(request):
+    secret_combination = [1, 2, 3, 4]
+    if request.method == "GET":
+        context = {
+            "result": None,
+            "message": "",
+        }
+        return render(request, "index.html")
+    else:
+        numbers_str = str(request.POST.get('numbers')).split()
+        if not Game.is_valid_count(numbers_str):
+            context = {
+                "result": 'error',
+                "message": "Please enter 4 number spearated by space",
+            }
+        elif not Game.is_unique_numbers(numbers_str):
+            context = {
+                "result": 'error',
+                "message": "Numbers are not unique",
+            }
+        elif not Game.is_valid_range(numbers_str):
+            context = {
+                "result": 'error',
+                "message": "Numbers must be 1-9",
+            }
+        else:
+            result = Game.play(numbers_str, secret_combination)
+            context = {
+                "result": 'success',
+                "message": f"Cows {len(result[0])} Bulls {len(result[1])}",
+            }
+
+    return render(request, "index.html", context)
